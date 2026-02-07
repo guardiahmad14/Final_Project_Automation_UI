1cde8606a572f6b84c894bddf9d299fd43921be9
Triguardi <triguardi.ahmad14@gmail.com>
2026-02-07 10:34:58 +0700

    Github Actions 8

--- src/test/java/AddDivisionTest.java
+++ src/test/java/AddDivisionTest.java
@@ -82,9 +82,9 @@ public class AddDivisionTest extends BaseTest {
     @Test
     public void add_division_test_failed(){
         log.info("Add Division Failed");
-        CredentialConfig CredentialsConfig = null;
-        String email = CredentialsConfig.EMAIL;
-        String password = CredentialsConfig.PASSWORD;
+        CredentialConfig credentialConfig = new CredentialConfig();
+        String email = credentialConfig.EMAIL;
+        String password = credentialConfig.PASSWORD;
         LoginPage loginPage = new LoginPage(DriverManager.getDriver());
         DashboardPage dashboardPage = new DashboardPage(DriverManager.getDriver());
         EmployeePage employeePage = new EmployeePage(DriverManager.getDriver());
