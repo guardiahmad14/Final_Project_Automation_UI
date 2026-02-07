53a47fb0fb5a691e8ddfd1c58f8af8c7203775ff
Triguardi <triguardi.ahmad14@gmail.com>
2026-02-07 11:24:21 +0700

    Github Actions 9

--- src/test/java/config/CredentialConfig.java
+++ src/test/java/config/CredentialConfig.java
@@ -1,7 +1,11 @@
 package config;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class CredentialConfig {
-    public static final String EMAIL = EnvConfig.get("EMAIL");
-    public static final String PASSWORD = EnvConfig.get("PASSWORD");
+        public static final String EMAIL = System.getenv("EMAIL");
+        public static final String PASSWORD = System.getenv("PASSWORD");
+    }
+//    public static final String EMAIL = EnvConfig.get("EMAIL");
+//    public static final String PASSWORD = EnvConfig.get("PASSWORD");
 //    public static final String COMPANY_ID = EnvConfig.get("COMPANY_ID");
-}
+//}
